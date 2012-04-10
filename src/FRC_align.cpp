0c1f9f925ffce4df56e532b73ead0bd236006bf4
vezzi <vezzi84@gmail.com>
2012-04-10 08:35:31 +0200

    ss

--- src/FRC_align.cpp
+++ src/FRC_align.cpp
@@ -387,8 +387,8 @@ int main(int argc, char *argv[]) {
 
 		//if(contigSize >= libraryPE.insertMean) {
 			frc.computeHighSpanningArea("PE", contig, currentContig, 1000, 200);
-			frc.computeCompressionArea("PE", contig, currentContig, -4.0, 2000, 200);
-			frc.computeStrechArea("PE", contig, currentContig, 4.0, 2000, 200);
+			frc.computeCompressionArea("PE", contig, currentContig, -3.0, 1000, 200);
+			frc.computeStrechArea("PE", contig, currentContig, 3.0, 1000, 200);
 			//}
 
     	delete currentContig; // delete hold contig
