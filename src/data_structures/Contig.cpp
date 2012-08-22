61cfdb7a92a1deb2b3b84b6df1e9b1a0157f129a
vezzi <vezzi84@gmail.com>
2012-08-22 13:30:44 +0200

    ss

--- src/data_structures/Contig.cpp
+++ src/data_structures/Contig.cpp
@@ -50,10 +50,10 @@ Contig::Contig() {
 	highSpanningFeat = 0.4;
 	highOutieFeat = 0.4;
 */
-	lowCoverageFeat = 1/(float)3;
-	highCoverageFeat = 1.6;
-	lowNormalFeat = 1/(float)3;
-	highNormalFeat = 1.6;
+	lowCoverageFeat = 1/(float)2.2;
+	highCoverageFeat = 2;
+	lowNormalFeat = 1/(float)2.2;
+	highNormalFeat = 2;
 	highSingleFeat = 0.4;
 	highSpanningFeat = 0.51;
 	highOutieFeat = 0.4;
@@ -65,10 +65,10 @@ Contig::Contig(unsigned int contigLength, unsigned int minInsert, unsigned int m
 	this->maxInsert = maxInsert;
 	this->CONTIG =  new Position[contigLength];
 
-	lowCoverageFeat = 1/(float)3;
-	highCoverageFeat = 1.6;
-	lowNormalFeat = 1/(float)3;
-	highNormalFeat = 1.6;
+	lowCoverageFeat = 1/(float)2.2;
+	highCoverageFeat = 2;
+	lowNormalFeat = 1/(float)2.2;
+	highNormalFeat = 2;
 	highSingleFeat = 0.4;
 	highSpanningFeat = 0.51;
 	highOutieFeat = 0.4;
